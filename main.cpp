73172e718ed04ff07382173e23aac47030c6bece
KwinTinn <lesha.streltsov@inbox.ru>
2023-04-08 03:50:56 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,116 +1,79 @@
 #include <iostream>
+#include "header.h"
 using namespace std;
-struct Node
-{
-    int weight = 0;
-    int id = 0;
-    Node *right = nullptr;
-    Node *down = nullptr;
-};
-
-class Graph
-{
-  private:
-  Node *top;
-  public:
-
-  Graph()
-  {
-      top = new Node();
-  }
-
-  Graph(Node *n1)
-  {
-      top = n1;
-  }
-
-  void connectNodes(Node *n1, Node *n2)
-  {
-      Node *start = top;
-      Node *move_ = top;
-      Node *move1;
-      Node *move2;
-      bool connected = false;
-      bool isSecondTopFound = false;
-      while (move_ != nullptr && connected == false)
-      {
-          if (move_->id == n1->id)
-          {
-              move1 = move_;
-              while(move1->right != nullptr)
-                move1 = move1->right; //Нашёл место для вставки связи
-          }
-          if (move_->id == n2->id)
-          {
-              //Нашёл вторую связуемую вершину в списке
-                isSecondTopFound = true;
-          }
-          if (isSecondTopFound == true && move1->right == nullptr)
-          {
-              move1->right = n2;
-              connected = true;
-          }
-          move_ = move_->down;
-      }
-      top = start;
-
-  }
-  void addNode(Node *n1)
-  {
-      Node *move;
-      move = top;
-      bool analogy = false;
-      while (move->down != nullptr)
-      {
-          if (move->id == n1->id) analogy = true;
-          move = move->down;
-      }
-      if (analogy != true)
-        move->down = n1;
-  }
-  void printNodes()
-  {
-      Node *moveRight;
-      Node *move = top;
-      while(move != nullptr)
-      {
-        moveRight = move;
-        cout << "Current node: [id] " << move->id << "; [weight] " << move->weight << endl;
-        cout << "Connected nodes:\n";
-        while(moveRight != nullptr)
-        {
-            if (moveRight->id != move->id)
-                cout << "[id] " << moveRight->id << "; [weight] " << moveRight->weight << endl;
-            moveRight = moveRight->right;
-        }
-        cout << "~~~~~~" << endl;
-        move = move->down;
-      }
-  }
-
-};
 
 
+/*
+    Код без разделений на файлы работает на OnlineGDB.
+    Если делить на файлы, OnlineGDB пишет ошибку линковщика.
+*/
 int main()
 {
-    Graph *g_map = new Graph();
-    Node *n = new Node();
-    n->weight = 1;
-    n->id = 2;
-    g_map->addNode(n);
-    Node *n1 = new Node();
-    n1->weight = 5;
-    n1->id = 55;
-    g_map->addNode(n1);
-    g_map->connectNodes(n, n1);
-    Node *n2 = new Node();
-    n2->id = 1488;
-    n2->weight = 228;
-    g_map->addNode(n2);
-    g_map->connectNodes(n1, n);
-    g_map->connectNodes(n2, n);
-    g_map->connectNodes(n2, n1);
-    g_map->printNodes();
+    setlocale(LC_ALL, "rus");
+     Graph <int> *g_map1 = new Graph <int>();
+    Node <int> *noda = new Node<int>();
+    for (int i = 1; i < 9; i = i + 1)
+    {
+        noda = new Node<int>();
+        noda->id = i;
+        noda->data = i*2;
+        g_map1->addNode(noda);
+    }
+    Node <int> *n1 = new Node<int>();
+    Node <int> *n2 = new Node<int>();
+    n1->id = 1;
+    n2->id = 3;
+    g_map1->conNode(n1, n2);
+    n2->id = 4;
+    g_map1->conNode(n1, n2);
 
-    return 0;
+
+    for (int i = 1; i <= 4; i++)
+    {
+        n1 = new Node<int>();
+        n2 = new Node<int>();
+        n1->id = i;
+        n2->id = i*2;
+        g_map1->conNode(n1, n2);
+    }
+
+       for (int i = 1; i <= 2; i++)
+       {
+        n1 = new Node<int>();
+        n2 = new Node<int>();
+        n1->id = i;
+        n2->id = i*2;
+        g_map1->delConNode(n1, n2);
+       }
+    n1 = new Node<int>();
+    n1->id = 1;
+    g_map1->delNode(n1);
+
+    g_map1->printMap(); 
+    
+    
+    
+    /*
+    Graph <char> *g_map1 = new Graph <char>();
+    Node <char> *noda = new Node<char>();
+    
+    noda->id = 'a';
+    noda->data = '+';
+    g_map1->addNode(noda);
+    
+    Node <char> *noda2 = new Node<char>();
+    noda2->id = 'b';
+    noda2->data = '-';
+    g_map1->addNode(noda2);
+    g_map1->conNode(noda, noda2);
+    g_map1->conNode(noda2, noda);
+    Node <char> *noda3 = new Node<char>();
+    noda3->id = 'z';
+    noda3->data = '@';
+    g_map1->addNode(noda3);
+            g_map1->conNode(noda, noda3);
+    g_map1->delConNode(noda, noda2);
+    g_map1->delNode(noda3);
+        g_map1->printMap();
+    */
 }
